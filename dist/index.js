ede2b94f75ca651937ce63d53cd2c97e34fd03f8
Phuc H <dshongphuc@gmail.com>
2018-07-07 11:42:39 +0700

    Add dist folder

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Stack = exports.Direction = exports.Card = undefined;
+
+var _Card = require('./Card');
+
+var _Card2 = _interopRequireDefault(_Card);
+
+var _Direction = require('./Direction');
+
+var _Direction2 = _interopRequireDefault(_Direction);
+
+var _Stack = require('./Stack');
+
+var _Stack2 = _interopRequireDefault(_Stack);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Card = _Card2.default;
+exports.Direction = _Direction2.default;
+exports.Stack = _Stack2.default; /**
+                                  * I created this repo to modify some code in the origin repo
+                                  */
